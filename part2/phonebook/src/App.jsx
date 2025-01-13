8c714ed0069ae177a9f8b036698d017999f28a94
aidang <maudan567@gmail.com>
2025-01-13 05:53:46 -1000

    2.11.2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,7 +3,7 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import { useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -13,13 +13,10 @@ const App = () => {
   const [newFilter, setNewFilter] = useState("");
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-      setPersons(response.data)
-    }
-  )
-}, [])
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value);
@@ -39,18 +36,15 @@ const App = () => {
       <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
       <h2>Add New</h2>
       <PersonForm
-        persons={persons} 
-        setPersons = {setPersons}
+        persons={persons}
+        setPersons={setPersons}
         newName={newName}
         newNumber={newNumber}
         handleNameChange={handleNameChange}
         handleNumberChange={handleNumberChange}
-       />
-      <h2>Numbers</h2>
-      <Persons
-        persons={persons}
-        query={newFilter}
       />
+      <h2>Numbers</h2>
+      <Persons persons={persons} query={newFilter} />
     </div>
   );
 };
